f09945a4d492cb1602da2edac3907c4faeee60ad
Jorgibiris <jorgeparedesperez@gmail.com>
2026-09-24 13:14:00 +0200

    Fase de agregar empleado e inclusión de navegación

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import ListadoEmpleados from './empleados/listadoEmpleados.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navegacion from './Navegacion.jsx'
+import ListadoEmpleados from './empleados/ListadoEmpleados.jsx'
+import AgregarEmpleado from './empleados/AgregarEmpleado.jsx'
+//import EditarEmpleado from './empleados/EditarEmpleado.jsx'
 
 export default function App() {
   return (
-    <div className="container py-4">
-      <header className="mb-4">
-        <h1 className="h3 mb-0">Sistema de Recursos Humanos</h1>
-        <p className="text-secondary mb-0">Listado de empleados</p>
-      </header>
-
-      <ListadoEmpleados />
-    </div>
+    <BrowserRouter>
+      <Navegacion />
+      <div className="container pb-4">
+        <Routes>
+          <Route path="/" element={<ListadoEmpleados />} />
+          <Route path="/agregar" element={<AgregarEmpleado />} />
+          {/*<Route path="/editar/:idEmpleado" element={<EditarEmpleado />} />*/}
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
